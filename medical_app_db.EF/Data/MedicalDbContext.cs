30604c98ae27e8eb33211a121922fcb8aa064fdd
RanaEbrahim24 <ebrahimabdelkader852@gmail.com>
2024-12-09 22:21:12 +0200

    first commit

---
+++ medical_app_db.EF/Data/MedicalDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace medical_app_db.EF.Data
+{
+    public class MedicalDbContext : DbContext
+    {
+        public MedicalDbContext(DbContextOptions<MedicalDbContext> options) : base(options)
+        {
+        }
+    }
+}
