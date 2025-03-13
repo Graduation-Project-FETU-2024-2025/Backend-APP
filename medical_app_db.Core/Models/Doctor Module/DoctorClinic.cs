4d4f46e05b68c61fcb5031271dc95136b56d2702
Khaled Atef <39334335+khaledatef2002@users.noreply.github.com>
2025-03-13 04:57:23 +0200

    doctor clinic

---
+++ medical_app_db.Core/Models/Doctor Module/DoctorClinic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace medical_app_db.Core.Models.Doctor_Module
+{
+	public class DoctorClinic
+	{
+		public Guid DoctorId { get; set; }
+		public Doctor Doctor { get; set; } = null!;
+		public Guid ClinicId { get; set; }
+
+		public Clinic Clinic { get; set; } = null!;
+	}
+}
