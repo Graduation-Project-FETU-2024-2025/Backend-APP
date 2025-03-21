05a9f830fdef203410b3f6e06b118e57f6f2ff3f
Hamza <hamza_31056248@f-eng.tanta.edu.eg>
2025-03-21 17:15:03 +0200

    adding Image Service & Updating Branch Controller

---
+++ medical_app_db.Core/Interfaces/IImageService.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Http;
+namespace medical_app_db.Core.Interfaces
+{
+    public interface IImageService
+    {
+        Task<string> UploadImageAsync(IFormFile? image);
+    }
+}
